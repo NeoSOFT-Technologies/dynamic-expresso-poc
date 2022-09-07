3e089e43714ef9ba65ab6c924df031ca5270bd00
NeoShivam <gupta.shivam@neosoftmail.com>
2022-09-07 12:53:00 +0530

    Added Dynamic Delegates and Lambda Expressions

--- src/DynamicExpressoPOC/Program.cs
+++ src/DynamicExpressoPOC/Program.cs
@@ -1,4 +1,20 @@
 ﻿using DynamicExpressoPOC.Features;
 
+PrintHeading("Identifier Detection");
 IdentifierDetections.IdentifierDetection();
+
+PrintHeading("Exception Handling");
 ExceptionsHandling.ExceptionHandling();
+
+
+PrintHeading("Dynamic Delegate");
+DynamicDelegates.DynamicDelegate();
+
+PrintHeading("Lambda Expression");
+LambdaExpressions.LambdaExpression();
+
+
+void PrintHeading(string topic)
+{
+    Console.WriteLine($"\n\n*******************************************{topic}*******************************************");
+}
